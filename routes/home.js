25bcc1b01227e25f8af4988ae3d8901f4b731c32
Kevin <ProFatXuanAll@gmail.com>
2018-03-08 22:24:10 +0800

    pug setup

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,40 @@ const express = require( 'express' );
 
 const route = express.Router();
 
-route.get( '', function( req, res ) {
+route.get( '/', urlSettings, function( req, res ) {
     res.render( 'index' );
 } );
+
+route.get( '/about', urlSettings, function( req, res ) {
+    res.render( 'about' );
+} );
+
+route.get( '/agenda', urlSettings, function( req, res ) {
+    res.render( 'agenda' );
+} );
+
+route.get( '/briefing', urlSettings, function( req, res ) {
+    res.render( 'briefing' );
+} );
+
+route.get( '/location', urlSettings, function( req, res ) {
+    res.render( 'location' );
+} );
+
+route.get( '/contact', urlSettings, function( req, res ) {
+    res.render( 'contact' );
+} );
+
+function urlSettings( req, res, next ) {
+    res.locals = {
+        home: '/',
+        about: '/about',
+        agenda: '/agenda',
+        briefing: '/briefing',
+        location: '/location',
+        contact: '/contact',
+    };
+    next();
+};
 
 module.exports = route;
