8daf822aac58310096feb6619868e0ea8813da85
Kevin <ProFatXuanAll@gmail.com>
2018-03-11 01:06:26 +0800

    add registration form

--- routes/home.js
+++ routes/home.js
@@ -5,19 +5,39 @@ const config = require( '../settings/server/config' );
 const route = express.Router();
 
 const urlSettings = ( req, res, next ) => {
-    let root = config.root;
-    res.locals = {
-        agenda: '/agenda',
-        contact: '/contact',
-        home: '/',
-        host: '/host',
-        location: '/location',
-        logoBlack: '/images/logoBlack.png',
-        logoWhite: '/images/logoWhite.png',
-        registration: '/registration',
-    };
-    for( const name in res.locals ) {
-        res.locals[name] = `${ root }${ res.locals[name] }`;
+    let root_path = config.root;
+    let static_path = config.static;
+    let url = {
+        root: {
+            agenda: '/agenda',
+            contact: '/contact',
+            home: '/',
+            host: '/host',
+            location: '/location',
+            registration: '/registration',
+        },
+        static: {
+            logoBlack: '/images/logoBlack.png',
+            logoWhite: '/images/logoWhite.png',
+        },
+    }
+    for( const name of Object.keys( url.root ) )
+    {
+        if( res.locals[ name ] ) {
+            throw new Error( `${name} already in res.locals` );
+        }
+        else {
+            res.locals[ name ] = `${ root_path }${ url.root[ name ] }`
+        }
+    }
+    for( const name of Object.keys( url.static ) )
+    {
+        if( res.locals[ name ] ) {
+            throw new Error( `${name} already in res.locals` );
+        }
+        else {
+            res.locals[ name ] = `${ static_path }${ url.static[ name ] }`
+        }
     }
     next();
 };
@@ -28,6 +48,11 @@ route.get( '/', urlSettings, function( req, res ) {
 
 route.get( '/registration', urlSettings, function( req, res ) {
     res.render( 'registration' );
+} );
+
+route.post( '/registration', urlSettings, function( req, res ) {
+    console.log( req.body.firstName );
+    res.render( 'success' );
 } );
 
 route.get( '/agenda', urlSettings, function( req, res ) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 
 const config = require( './settings/server/config' );
 const home = require( './routes/home' );
@@ -9,5 +10,7 @@ server.listen( config.port );
 
 server.set( 'view engine', 'pug' );
 
+server.use( bodyParser.json() );
+server.use( bodyParser.urlencoded( { extended: true } ) );
 server.use( config.static, express.static( 'static/dist' ) );
 server.use( config.root, home );
