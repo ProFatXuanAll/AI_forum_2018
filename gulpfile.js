c3512a5ddf1dcf5829719ef68b8a971ca30f7969
Kevin <ProFatXuanAll@gmail.com>
2018-03-12 00:36:20 +0800

    gulp sass setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 const gulp = require( 'gulp' );
 
 const eslint = require( 'gulp-eslint' );
+const sass = require( 'gulp-sass' );
 const nodemon = require( 'gulp-nodemon' );
 
 // lint task
 gulp.task( 'eslint_backend', function() {
-    return gulp.src( [ '**/*.js', '!node_modules/**', '!static/**' ] )
-        .pipe( eslint( { configFile: 'settings/lint/eslint/backend.json' } ) )
+    return gulp.src( [ './**/*.js', '!node_modules/**', '!./static/**' ] )
+        .pipe( eslint( { configFile: './settings/lint/eslint/backend.json' } ) )
         .pipe( eslint.format() );
 } );
 gulp.task( 'eslint_frontend', function() {
-    return gulp.src( [ 'static/src/**/*.js' ] )
-        .pipe( eslint( { configFile: 'settings/lint/eslint/frontend.json' } ) )
+    return gulp.src( [ './static/src/**/*.js' ] )
+        .pipe( eslint( { configFile: './settings/lint/eslint/frontend.json' } ) )
         .pipe( eslint.format() );
 } );
 
+// sass task
+gulp.task( 'sass', function() {
+    return gulp.src( './static/src/sass/**/*.sass' )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( './static/dist/css' ) )
+} );
+
+gulp.task( 'sass:watch', function() {
+    gulp.watch( './sass/**/*.sass', [ 'sass' ] )
+} );
+
 // nodemon
-gulp.task( 'start', function() {
+gulp.task( 'start', [ 'sass' ], function() {
     nodemon( {
         ext: 'js pug sass',
         script: 'server.js'
